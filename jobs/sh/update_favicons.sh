9e474d326ead0928461770353fd294c44f97efee
hypothermic <admin@hypothermic.nl>
2018-08-01 02:29:55 +0200

    Add partial favicon support (with pre-set images and values)

---
+++ jobs/sh/update_favicons.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cd "$(dirname "$0")"
+
+# TO-DO: replace all links in the favicon configurations (favicons.php, browserconfig.xml, html_code.html, site.webmanifest) with dir path in wiki.xml.
--- templates/meta.php
+++ templates/meta.php
@@ -1,3 +1,4 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <?php include('fonts.php');?>
+<?php include('favicons.php');?>
